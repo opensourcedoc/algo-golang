80ef211384cbbeaccc1ae3a62090273c69e9941b
Michael Chen <cwchen123@gmail.com>
2017-05-19 02:17:42 +0000

    Refactor code

--- list/functor_reduce_test.go
+++ list/functor_reduce_test.go
@@ -5,20 +5,6 @@ import (
 	"testing"
 )
 
-func add(a interface{}, b interface{}) (interface{}, error) {
-	_a, ok := a.(int)
-	if ok != true {
-		return nil, errors.New("Failed type assertion on a")
-	}
-
-	_b, ok := b.(int)
-	if ok != true {
-		return nil, errors.New("Failed type assertion on b")
-	}
-
-	return _a + _b, nil
-}
-
 func TestReduce(t *testing.T) {
 	list := new1To10List()
 
@@ -36,4 +22,18 @@ func TestReduceSingle(t *testing.T) {
 	if result != 100 {
 		t.Error("Error Reduce on single element")
 	}
+}
+
+func add(a interface{}, b interface{}) (interface{}, error) {
+	_a, ok := a.(int)
+	if ok != true {
+		return nil, errors.New("Failed type assertion on a")
+	}
+
+	_b, ok := b.(int)
+	if ok != true {
+		return nil, errors.New("Failed type assertion on b")
+	}
+
+	return _a + _b, nil
 }
