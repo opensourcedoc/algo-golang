ee35f0999c731bc228731a36fe253ed096ae66ed
Michael Chen <cwchen123@gmail.com>
2017-05-20 03:36:01 +0000

    Update benchmark

--- list/functor_map.go
+++ list/functor_map.go
@@ -9,19 +9,12 @@ func (list *List) Map(m func(interface{}) (interface{}, error)) (*List, error) {
 
 	current := list.head
 	for current != nil {
-		cnum := make(chan interface{}, 1)
-		cerr := make(chan error, 1)
-		go func() {
-			d, e := m(current.data)
-			cnum <- d
-			cerr <- e
-		}()
-
-		err := <-cerr
+		num, err := m(current.data)
 		if err != nil {
 			return newList, err
 		}
-		newList.Push(<-cnum)
+
+		newList.Push(num)
 
 		current = current.next
 	}
