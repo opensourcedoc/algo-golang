3de333004e97794a414d901c27762dced6c66ad4
Michael Chen <cwchen.tw@outlook.com>
2017-09-12 21:07:01 +0800

    Add documention for float64 vector

--- vector/float64/vector.go
+++ vector/float64/vector.go
@@ -4,16 +4,20 @@ import (
 	"math"
 )
 
+// IVector is the interface of Vector class.
 type IVector interface {
 	Len() int
 	GetAt(int) float64
 	SetAt(int, float64)
 }
 
+// Vector class implements IVector interface.
+// Internally, it stores data into a float64 slice.
 type Vector struct {
 	vec []float64
 }
 
+// New creates a vector based on arguments.
 func New(args ...float64) IVector {
 	v := new(Vector)
 	v.vec = make([]float64, len(args))
@@ -26,6 +30,7 @@ func New(args ...float64) IVector {
 	return v
 }
 
+// WithSize creates a zero-based vector with specific size.
 func WithSize(size int) IVector {
 	v := new(Vector)
 	v.vec = make([]float64, size)
@@ -37,6 +42,7 @@ func WithSize(size int) IVector {
 	return v
 }
 
+// FromArray creates a vector from float64 slice.
 func FromArray(arr []float64) IVector {
 	v := new(Vector)
 	v.vec = make([]float64, len(arr))
@@ -48,12 +54,12 @@ func FromArray(arr []float64) IVector {
 	return v
 }
 
-// The length of the vector
+// Len returns the length of the vector. Compare it with Magnitude.
 func (v *Vector) Len() int {
 	return len(v.vec)
 }
 
-// Getter
+// GetAt returns the value of the vector at index i.
 func (v *Vector) GetAt(i int) float64 {
 	if i < 0 || i >= v.Len() {
 		panic("Index out of range")
@@ -62,7 +68,7 @@ func (v *Vector) GetAt(i int) float64 {
 	return v.vec[i]
 }
 
-// Setter
+// SetAt sets the value of the vector at index i.
 func (v *Vector) SetAt(i int, data float64) {
 	if i < 0 || i >= v.Len() {
 		panic("Index out of range")
@@ -71,12 +77,14 @@ func (v *Vector) SetAt(i int, data float64) {
 	v.vec[i] = data
 }
 
+// Magnitude returns the magnitude of a vector.
 func Magnitude(v IVector) float64 {
 	temp := Map(v, func(n float64) float64 { return math.Pow(n, 2) })
 	sum := Reduce(temp, func(a float64, b float64) float64 { return a + b })
 	return math.Sqrt(sum)
 }
 
+// Sort sorts a vector.
 func Sort(v IVector) IVector {
 	if v.Len() == 0 {
 		return v
@@ -109,74 +117,82 @@ func Sort(v IVector) IVector {
 	return FromArray(arr)
 }
 
-// Vector addition
+// Add performs element-wise addition between two vectors.
 func Add(v1 IVector, v2 IVector) IVector {
 	return Apply(v1, v2, func(a float64, b float64) float64 { return a + b })
 }
 
+// ScalarAdd adds a vector and a scalar.
 func ScalarAdd(v IVector, s float64) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return a + b })
 }
 
-// Vector substraction
+// Sub performs element-wise substraction between two vectors.
 func Sub(v1 IVector, v2 IVector) IVector {
 	return Apply(v1, v2, func(a float64, b float64) float64 { return a - b })
 }
 
+// ScalarSubFirst substracts a scalar to a vector.
 func ScalarSubFirst(s float64, v IVector) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return b - a })
 }
 
+// ScalarSubSecond substracts a vector to a scalar.
 func ScalarSubSecond(v IVector, s float64) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return a - b })
 }
 
-// Vector multiplication
+// Mul performs element-wise multiplication between two vectors.
 func Mul(v1 IVector, v2 IVector) IVector {
 	return Apply(v1, v2, func(a float64, b float64) float64 { return a * b })
 }
 
+// ScalarMul performs element-wise multiplication between a vector and a scalar.
 func ScalarMul(v IVector, s float64) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return a * b })
 }
 
-// Vector division
+// Div perfomrms element-wise division.
 func Div(v1 IVector, v2 IVector) IVector {
 	return Apply(v1, v2, func(a float64, b float64) float64 { return a / b })
 }
 
+// ScalarDivFirst divides a scalar numerator and a vector denominator.
 func ScalarDivFirst(s float64, v IVector) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return b / a })
 }
 
+// ScalarDivSecond divides a vector numerator and a scalar denominator.
 func ScalarDivSecond(v IVector, s float64) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return a / b })
 }
 
-// Vector power
+// Pow performs element-wise power.
 func Pow(v1 IVector, v2 IVector) IVector {
 	return Apply(v1, v2, func(a float64, b float64) float64 { return math.Pow(a, b) })
 }
 
+// ScalarPowBase return the power of a scalar base against a vector exponent.
 func ScalarPowBase(s float64, v IVector) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return math.Pow(b, a) })
 }
 
+// ScalarPowExp return the power of a vector base against a scalar exponent.
 func ScalarPowExp(v IVector, s float64) IVector {
 	return ScalarApply(v, s, func(a float64, b float64) float64 { return math.Pow(a, b) })
 }
 
-// Vector product
+// Dot returns the dot product between two vectors.
 func Dot(v1 IVector, v2 IVector) float64 {
 	return Reduce(Mul(v1, v2), func(a float64, b float64) float64 { return a + b })
 }
 
+// Cos returns the cosine value between two vectors.
 func Cos(v1 IVector, v2 IVector) float64 {
 	return Dot(v1, v2) / (Magnitude(v1) * Magnitude(v2))
 }
 
-// Vector transformation delegating to function object.
-// This method delegates vector transformation to function object set by users.
+// Map transform a vector into another vector by an user-defined function.
 func Map(v IVector, f func(float64) float64) IVector {
 	_len := v.Len()
 
@@ -189,9 +205,7 @@ func Map(v IVector, f func(float64) float64) IVector {
 	return out
 }
 
-// Vector algebra delegating to function object.
-// This method delegates vector algebra to function object set by users, making
-// it faster then these methods relying on reflection.
+// Apply applies an user-defined function to two vectors.
 func Apply(v1 IVector, v2 IVector, f func(float64, float64) float64) IVector {
 	_len := v1.Len()
 
@@ -208,6 +222,7 @@ func Apply(v1 IVector, v2 IVector, f func(float64, float64) float64) IVector {
 	return out
 }
 
+// ScalarApply applies an user-defined function to a vector and a scalar.
 func ScalarApply(v IVector, s float64, f func(float64, float64) float64) IVector {
 	_len := v.Len()
 
@@ -220,8 +235,7 @@ func ScalarApply(v IVector, s float64, f func(float64, float64) float64) IVector
 	return out
 }
 
-// Vector to scalars reduction.
-// This method delegates vector reduction to function object set by users.
+// Reduce tranforms a vector to a scalar by an user-defined function.
 func Reduce(v IVector, f func(float64, float64) float64) float64 {
 	_len := v.Len()
 
